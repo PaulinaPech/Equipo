606f270cf9595ef2fec78f7043c4c58f1cebcdd1
unknown <paupech9795@gmail.com>
2022-10-20 17:05:04 -0500

    Se agrega el avance

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+/*Grupo : TI 4A. Equipo: 
+Stephanie Paulina Pech Cervera
+Samir Ivan Molina Arredondo
+Alejandro Martin Caamal
+Antonio Chi Moo
+Juan Romero Yam
+*/
 var mysql = require('mysql');
 var express = require('express');
 
 var app = express();
+app.use(express.json());
+
+const puerto=process.env.PUERTO || 7002;
 
 //Conexión de la base de datos 
 var conexion = mysql.createConnection({
@@ -18,33 +28,15 @@ if (error){
 }else{
     console.log('Conexion exitosa');
 }
-})
-/*Mostrar-Prueba uno
-conexion.query('SELECT * FROM Producto', function (error,filas){
-    if(error){
-        throw error;
-    }else{
-        filas.forEach(fila => {
-            console.log(fila);
-        });
-    }
-});*/
-
-//GET
-app.get('/', function(req,res){
-   res.send('Ruta principal');
 });
 
-const puerto=process.env.PUERTO;
-app.listen(puerto, function(){
+//GET- Mostrar
+app.listen(puerto, ()=>{
 console.log("Servidor ok en puerto:"+puerto);
 });
 
-
-
-/*
 app.get('/api/Clase3', (req,res)=>{
-  conexion.query('SELECT * FROM Producto', (error, filas)=>{
+   conexion.query('SELECT * FROM Articulo', (error, filas)=>{
      if(error){
         throw error;
      }else{
@@ -52,10 +44,55 @@ app.get('/api/Clase3', (req,res)=>{
      }
   })
 });
-Insertar-Prueba dos
-conexion.query('INSERT into Producto (idProducto, nombreProducto, precio, vigente, idCategoria_FK) VALUES (4, "Samsumg g350", 500, 1, 1004)', function(error, results){
-if(error) throw error;
-console.log('Registro Agregado'. results)
-});*/
 
-conexion.end();
+app.get('/api/Clase3/:idArticulo', (req,res)=>{
+   conexion.query('SELECT * FROM Articulo WHERE idArticulo = ?',[req.params.idArticulo], (error, fila)=>{
+     if(error){
+        throw error;
+     }else{
+        res.send(fila);
+     }
+  })
+});
+
+//POST-Insertar
+app.post('/api/Clase3',(req,res)=>{
+  let data= {nombreArticulo:req.body.nombreArticulo,precio:req.body.precio,stock:req.body.stock}
+  let sql = "INSERT INTO Articulo SET ?";
+  conexion.query(sql, data, function(error, results){
+    if(error){
+        throw error;
+     }else{
+        res.send(results);
+     }
+  });
+});
+
+
+//PUT-Editar
+
+app.put('/api/Clase3/:idArticulo', (req,res)=>{
+   let idArticulo = req.params.idArticulo;
+   let nombreArticulo= req.body.nombreArticulo;
+   let precio=req.body.precio;
+   let stock=req.body.stock;
+   let sql="UPDATE Articulo SET nombreArticulo = ?, precio = ?, stock = ? WHERE idArticulo = ?";
+   conexion.query(sql, [nombreArticulo, precio, stock, idArticulo],function(error, results){
+    if(error){
+        throw error;
+     }else{
+        res.send(results);
+     }
+   });
+});
+
+//DELETE-Eliminar 
+app.delete('/api/Clase3/:idArticulo', (req,res)=>{
+      conexion.query('DELETE FROM Articulo WHERE idArticulo = ?', [req.params.idArticulo], function(error,filas){
+         if(error){
+            throw error;
+         }else{
+            res.send(filas);
+         }
+      });
+});
